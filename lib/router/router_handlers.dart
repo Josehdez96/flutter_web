1f33a8ccda83e83b267d934fb817dffe3c8aad9c
josed.hernandez <josed.hernandez@pragma.com.co>
2021-08-24 08:18:18 -0500

    initial commit

---
+++ lib/router/router_handlers.dart
@@ -0,0 +1,11 @@
+
+import 'dart:js';
+
+import 'package:fluro/fluro.dart';
+import 'package:vertical_landing_page/ui/pages/home_page.dart';
+
+final homeHandler = Handler(
+  handlerFunc: (context, params) {
+    
+    return HomePage();
+});
