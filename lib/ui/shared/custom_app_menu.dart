667ffa9b6618f487efa6003b30b4738e5bcdff68
josed.hernandez <josed.hernandez@pragma.com.co>
2021-08-25 08:07:43 -0500

    feat(custom_app_menu): menu with its controller

---
+++ lib/ui/shared/custom_app_menu.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomAppMenu extends StatefulWidget {
+
+
+  @override
+  _CustomAppMenuState createState() => _CustomAppMenuState();
+}
+
+class _CustomAppMenuState extends State<CustomAppMenu> with SingleTickerProviderStateMixin {
+
+  bool isOpen = false;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this, duration: Duration( milliseconds: 200 ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: () {
+
+          if ( isOpen ) {
+            controller.reverse();
+          } else {
+            controller.forward();
+          }
+
+          setState(() {
+            isOpen = !isOpen;
+          });
+        },
+        child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            width: 150,
+            height: 50,
+            color: Colors.black,
+            child: Row(
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 200),
+                  curve: Curves.easeInOut,
+                  width: isOpen ? 40 : 0,
+                ),
+                Text(
+                  'Menú',
+                  style: GoogleFonts.roboto(
+                    color: Colors.white,
+                    fontSize: 18
+                  ),
+                ),
+                Spacer(),
+                AnimatedIcon(
+                  icon: AnimatedIcons.menu_close,
+                  progress: controller,
+                  color: Colors.white,
+                )
+              ]
+            ),
+        ),
+      ),
+    );
+  }
+}
