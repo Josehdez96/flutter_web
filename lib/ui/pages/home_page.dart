2bc07e07ab98ebd13055afd2293bf4e996a1d84e
josed.hernandez <josed.hernandez@pragma.com.co>
2021-08-25 07:08:14 -0500

    feat(views): views and gradient

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_landing_page/ui/views/about_view.dart';
+import 'package:vertical_landing_page/ui/views/contact_view.dart';
+import 'package:vertical_landing_page/ui/views/home_view.dart';
+import 'package:vertical_landing_page/ui/views/location_view.dart';
+import 'package:vertical_landing_page/ui/views/pricing_dart.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      color: Colors.pink,
+    return Scaffold(
+      body: Container(
+        decoration: buildBoxDecoration(),
+        child: Stack(
+          children: [
+            _HomeBody(),
+
+            Positioned(
+              right: 20,
+              top: 20,
+              child: Container(
+                width: 150,
+                height: 50,
+                color: Colors.black,
+              )
+            )
+          ]
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration buildBoxDecoration() => BoxDecoration(
+    gradient: LinearGradient(
+      colors: [
+        Colors.pink,
+        Colors.purpleAccent
+      ],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      stops: [ 0.5, 0.5 ]
+    )
+  );
+}
+
+class _HomeBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      children: [
+        HomeView(),
+        AboutView(),
+        PricingView(),
+        ContactView(),
+        LocationView()
+      ]
     );
   }
 }
