efe421593f2706a30c01b7413bb0413e4551f5a3
josed.hernandez <josed.hernandez@pragma.com.co>
2021-08-26 07:02:15 -0500

    feat(page_provider): scroll with menu options

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart';
 
 class PageProvider extends ChangeNotifier {
 
   PageController scrollController = new PageController();
 
+  List<String> _pages = ['home', 'about', 'pricing', 'contact', 'location'];
+
   goTo(int index) {
+    window.history.pushState(null, '', '#/${ _pages[index] }');
+
     scrollController.animateToPage(index,
     duration: Duration(milliseconds: 300),
     curve: Curves.easeInOut
