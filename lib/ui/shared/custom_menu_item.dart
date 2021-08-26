0d2048a688dfa8bd6b3ef90e918f59b878bffc7d
josed.hernandez <josed.hernandez@pragma.com.co>
2021-08-26 06:29:27 -0500

    chore: animated_do lib added

--- lib/ui/shared/custom_menu_item.dart
+++ lib/ui/shared/custom_menu_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:animate_do/animate_do.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomMenuItem extends StatefulWidget {
@@ -22,22 +24,24 @@ class _CustomMenuItemState extends State<CustomMenuItem> {
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onEnter: ( _ ) => setState(() => isHover = true),
-      onExit: ( _ ) => setState(() => isHover = false),
-      child: GestureDetector(
-        onTap: () => widget.onPressed(),
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: 300),
-          width: 150,
-          height: 50,
-          color: isHover ? Colors.pinkAccent : Colors.black,
-          child: Center(
-            child: Text(
-              widget.text,
-              style: GoogleFonts.roboto(fontSize: 20, color: Colors.white)
-            ),
-          )
+    return FadeIn(
+      child: MouseRegion(
+        onEnter: ( _ ) => setState(() => isHover = true),
+        onExit: ( _ ) => setState(() => isHover = false),
+        child: GestureDetector(
+          onTap: () => widget.onPressed(),
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            width: 150,
+            height: 50,
+            color: isHover ? Colors.pinkAccent : Colors.black,
+            child: Center(
+              child: Text(
+                widget.text,
+                style: GoogleFonts.roboto(fontSize: 20, color: Colors.white)
+              ),
+            )
+          ),
         ),
       ),
     );
